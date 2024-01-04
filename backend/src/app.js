089be50d01fc84d2b34bd806bc7ac705b76e30fb
dylancdavis <dylcdav@gmail.com>
2024-01-04 17:34:48 -0500

    Add last-clicked endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/last-clicked", (req, res) => {
+  return Date("2024-01-01");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
