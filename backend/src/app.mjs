52371f77b3c1b6cdac8ba0338d02730d85ec6170
dylancdavis <dylcdav@gmail.com>
2024-01-04 20:27:45 -0500

    Update GET for last-clicked

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -22,7 +22,8 @@ app.get("/", (req, res) => {
 app.get("/last-clicked", (req, res) => {
   const db = new LowSync(new JSONFileSync("db.json"), {});
   db.read();
-  res.send(db.data);
+  const dateToSend = db.data.lastClicked ?? db.data.startingTime;
+  res.send(JSON.stringify(dateToSend));
 });
 
 app.post("/last-clicked", (req, res) => {
