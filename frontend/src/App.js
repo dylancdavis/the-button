4366415106c58e16b3a5a45caea3d3f8e859dcdb
dylancdavis <dylcdav@gmail.com>
2024-01-05 15:57:44 -0500

    Make URL relative

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import "./reset.css";
 
-const baseURL = "http://localhost:8080";
-const apiURL = `${baseURL}/api`;
+// const baseURL = "http://localhost:8080";
+const apiURL = `/api`;
 
 function App() {
   const buttonLifeSpan = 1000 * 60 * 60 * 24 * 7 * 2; // Two Weeks
