5828dd000a5c38a81e2abec3739fb4a323d95304
dylancdavis <dylcdav@gmail.com>
2024-01-04 20:12:06 -0500

    Replace last-clicked with last-clicked-db

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -10,14 +10,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/last-clicked-db", (req, res) => {
-  const db = new LowSync(new JSONFileSync("db.json"));
+app.get("/last-clicked", (req, res) => {
+  const db = new LowSync(new JSONFileSync("db.json"), {});
   db.read();
   res.send(db.data);
-});
-
-app.get("/last-clicked", (req, res) => {
-  res.send(new Date("2024-01-01"));
 });
 
 app.listen(port, () => {
