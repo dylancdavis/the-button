3387c8261d603b51b6944a6a8dcb5fdab63468d0
dylancdavis <dylcdav@gmail.com>
2024-01-04 19:55:34 -0500

    Add last-clicked-db

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+import { LowSync } from "lowdb";
+import { JSONFileSync } from "lowdb/node";
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get.get("/last-clicked-db", (req, res) => {
+  const db = new LowSync(new JSONFileSync("db.json"));
+  db.read();
+  res.send(db.data);
+});
+
 app.get("/last-clicked", (req, res) => {
-  return Date("2024-01-01");
+  res.send(Date("2024-01-01"));
 });
 
 app.listen(port, () => {
