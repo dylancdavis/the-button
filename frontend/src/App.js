afc207e964855b5b1e370f26c2b456f7cf39b4c5
Cole D <104338788+dylancdavis@users.noreply.github.com>
2024-09-19 12:12:44 +0000

    Add release date

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="goodbye-message">the button will return</div>
+      <div className="goodbye-message">the button will return 2024.10.01</div>
     </div>
   );
 }
