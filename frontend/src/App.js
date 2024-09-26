8dbe11a9774849dd152600e76e9d92ad1552e656
Cole D <104338788+dylancdavis@users.noreply.github.com>
2024-09-26 11:26:59 +0000

    Move new ButtonPage to beta path in router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ButtonPage from "./ButtonPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./app.css";
 import "./reset.css";
 
 function App() {
   return (
     <div className="App">
-      <ButtonPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/beta" element={<ButtonPage />} />
+        </Routes>
+      </BrowserRouter>
       <div className="app-version">v2.0</div>
     </div>
   );
